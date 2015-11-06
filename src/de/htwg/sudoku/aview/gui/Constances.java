2031a33d6aab5ada193dfa3efb94887974055a08
marko.boger@gmail.com <marko.boger@gmail.com>
2015-11-06 09:43:24 +0100

    changed gradle to export pom and fixed a few findbugs

--- src/de/htwg/sudoku/aview/gui/Constances.java
+++ src/de/htwg/sudoku/aview/gui/Constances.java
@@ -3,6 +3,10 @@ package de.htwg.sudoku.aview.gui;
 import java.awt.Color;
 
 public final class Constances {
+	
+	private Constances(){
+		//hide public constructor
+	}
     
     /** The color used to fill cells that are given at the start of the game. */
     public static final Color GIVEN_BACKGROUND_COLOR = new Color(200, 200, 255);
